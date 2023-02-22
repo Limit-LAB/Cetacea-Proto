c02fd533af12010c1e9ba6b19a272e4e55e1fb32
lemonhx <lemonhx@lemonhx.moe>
2023-02-22 10:27:20 +0000

    extensible and typed

--- src/extensions/room_v1.rs
+++ src/extensions/room_v1.rs
@@ -1,6 +1,6 @@
 //! impl of [`crate::extensions::Extensions::RoomV1`] required datatypes.
 
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize, Serializer};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct InheritRoomFromV1 {
@@ -36,6 +36,7 @@ pub struct TextMessagePartV1 {
     encryption_method: Option<EncryptionMethod>,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct ReactionMessagePartV1 {
     /// the event id.
     react_to: String,
@@ -50,5 +51,64 @@ pub struct MessageV1 {
     /// pin the message to the top of the room.
     pin: bool,
     /// the message parts.
-    message_parts: Vec<serde_json::Value>,
+    message_parts: Vec<MessagePartV1>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum MessagePartV1 {
+    Supported(MessagePartV1Inner),
+    Other {
+        #[serde(rename = "type")]
+        type_: String,
+        #[serde(flatten)]
+        data: serde_json::Value,
+    },
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "type")]
+pub enum MessagePartV1Inner {
+    Text(TextMessagePartV1),
+    Reaction(ReactionMessagePartV1),
+}
+
+#[test]
+fn test_extensibility() {
+    #[derive(Debug, Serialize, Deserialize)]
+    struct DemoMessage {
+        a: String,
+        b: Option<Vec<u8>>,
+        c: Result<(), String>,
+    }
+    let demo = DemoMessage {
+        a: "hello".to_string(),
+        b: None,
+        c: Ok(()),
+    };
+    let messagev1 = MessageV1 {
+        reply_to: None,
+        pin: false,
+        message_parts: vec![
+            MessagePartV1::Other {
+                type_: "demo".to_string(),
+                data: serde_json::to_value(demo).unwrap(),
+            },
+            MessagePartV1::Supported(MessagePartV1Inner::Text(TextMessagePartV1 {
+                text: "hello".to_string(),
+                encryption_method: None,
+            })),
+            MessagePartV1::Supported(MessagePartV1Inner::Reaction(ReactionMessagePartV1 {
+                react_to: "123".to_string(),
+                reaction: "👍".to_string(),
+            })),
+        ],
+    };
+
+    let rmp = rmp_serde::to_vec_named(&messagev1).unwrap();
+    println!("{:?}", rmp);
+    let messagev1: serde_json::Value = rmp_serde::from_read_ref(&rmp).unwrap();
+    println!("{:#?}", messagev1);
+    let messagev1: MessageV1 = rmp_serde::from_read_ref(&rmp).unwrap();
+    println!("{:#?}", messagev1);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![feature(associated_type_defaults)]
+#![feature(const_type_name)]
 
 pub mod client;
 pub mod server;
