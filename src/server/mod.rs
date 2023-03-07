67e5a3bd8b6436ada9ace51d9ad1086ba2e6dd0a
imlyzh <enterindex@gmail.com>
2023-03-07 20:21:57 +0800

    added IO struct

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,3 +1,4 @@
 pub mod discovery;
 pub mod events;
 pub mod sync;
+pub mod io;
--- src/server/sync.rs
+++ src/server/sync.rs
@@ -8,16 +8,21 @@ struct Message {
   // todo
 }
 
+
 #[derive(Debug, Serialize, Deserialize)]
-struct UserId {
-  // uuid
-}
+struct UUID();
 
 
 #[derive(Debug, Serialize, Deserialize)]
-struct ChannelId {
-  // uuid
-}
+struct ServerName();
+
+
+#[derive(Debug, Serialize, Deserialize)]
+struct UserId (pub UUID, pub ServerName);
+
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ChannelId (pub UUID, pub ServerName);
 
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -27,13 +32,28 @@ enum SubjectId {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct MessageId {
-  // uuid
-}
+struct MessageId (pub UUID);
+
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Signature (pub String);
+
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct MessagePushRequest {
   prev_message_id: MessageId,
+  signature: Signature,
+  message: Message,
+  sender: UserId,
+  reciver: SubjectId,
+}
+
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MessagePushResponse {
+  prev_message_id: MessageId,
+  signature: Signature,
+  origin: ServerName,
   message: Message,
   sender: UserId,
   reciver: SubjectId,
