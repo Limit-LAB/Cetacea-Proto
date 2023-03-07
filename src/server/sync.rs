3ec0cff43e85906075aff330a3ee6236245822d4
imlyzh <enterindex@gmail.com>
2023-03-07 19:50:52 +0800

    add message push request

--- src/server/sync.rs
+++ src/server/sync.rs
@@ -1,1 +1,40 @@
 #![doc = include_str!("../../docs/server_sync.md")]
+
+use serde::{Deserialize, Serialize};
+
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Message {
+  // todo
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct UserId {
+  // uuid
+}
+
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ChannelId {
+  // uuid
+}
+
+
+#[derive(Debug, Serialize, Deserialize)]
+enum SubjectId {
+  User(UserId),
+  ChannelId(ChannelId),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct MessageId {
+  // uuid
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MessagePushRequest {
+  prev_message_id: MessageId,
+  message: Message,
+  sender: UserId,
+  reciver: SubjectId,
+}
